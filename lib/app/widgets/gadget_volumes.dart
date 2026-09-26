1cfae357828d4b301c8906f1b2c109ea6901f75e
Oliver Grawert <ogra@ubuntu.com>
2026-09-26 13:42:01 +0200

    make sure the new volume name is properly propagated to all widgets by calling a refresh

--- lib/app/widgets/gadget_volumes.dart
+++ lib/app/widgets/gadget_volumes.dart
@@ -187,6 +187,9 @@ class _GadgetVolumesSectionState extends State<GadgetVolumesSection> {
         // Write back to file
         file.writeAsStringSync(editor.toString());
         widget.onStatusUpdate!('Volume name updated successfully');
+        if (widget.refreshCallback != null) {
+          widget.refreshCallback!();
+        }
       }
 
     } catch (e) {
